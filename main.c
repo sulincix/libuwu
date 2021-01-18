2ec17bf080349f23b2700e87347604f4c8716d86
Your Name <you@example.com>
2021-01-18 09:33:30 +0000

    fix segmentation fault

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <libuwu.h>
 
-int main(int angc,char *argv[]){
+int main(int argc,char *argv[]){
     char*data=readlines(argv[1]);
-    char *v=get_node(data,argv[2]);
+    char *v;
+    if(argc>2)
+        v=get_node(data,argv[2]);
+    else
+        v=get_node(data,"");
     puts(v);
 }
